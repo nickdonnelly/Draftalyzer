89ea283056e55c1dd9a1593b50eaa392e4adbb22
Nicholas Donnelly <ndonnelly1006@gmail.com>
2016-05-10 17:44:24 +0200

    Added some DefinitionUpdater stuff

--- draft/definition_updater.go
+++ draft/definition_updater.go
@@ -18,6 +18,7 @@ const (
 type DefinitionUpdater struct{
   mode DefinitionUpdaterMode
   status DefinitionUpdaterStatus // will default to idle thanks to iota
+  heroes []string
 }
 
 func (updater *DefinitionUpdater) getMode() DefinitionUpdaterMode{
@@ -25,6 +26,7 @@ func (updater *DefinitionUpdater) getMode() DefinitionUpdaterMode{
 }
 
 func (updater *DefinitionUpdater) setMode(newMode DefinitionUpdaterMode) bool{
+  updater.Reset()
   updater.mode = newMode
   if updater.mode == newMode {
     return true
@@ -37,7 +39,30 @@ func (updater *DefinitionUpdater) getStatus() DefinitionUpdaterStatus{
   return updater.status
 }
 
+func (updater *DefinitionUpdater) Reset() bool{
+  if(updater.status == STATUS_IDLE || updater.status == STATUS_COMPLETE){
+    updater.mode = ALL_HEROES
+    updater.heroes = nil
+    updater.status = STATUS_IDLE
+    return true
+  }else{
+    return false
+  }
+}
 
 func (updater *DefinitionUpdater) Update() bool{
   return false // false = not succesful
+}
+
+func (updater *DefinitionUpdater) AddHero(heroName string) bool {
+  if updater.mode == ALL_HEROES{
+    return false
+  }else if updater.mode == SINGLE_HERO && len(updater.heroes) >= 1{
+    // error out if they are trying to append to the list and
+    // they selected single hero mode
+    return false
+  }else{
+    updater.heroes = append(updater.heroes, heroName)
+    return true
+  }
 }
